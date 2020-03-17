d29e5e6c7b5b44f8b5de4ad7bd6a2356b2c8fa4b
Pete James <petejames@petes-air.lan>
2020-03-17 14:32:45 -0700

    logger middleware added

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const server = require('./server.js'); // like a react import from another file
+
+server.listen(4000, () => {
+    console.log('listening on port 4000....')
+});
+
+
+
--- server.js
+++ server.js
@@ -1,13 +1,80 @@
 const express = require('express');
+// const morgan = require('morgan');
+
 
 const server = express();
+
+// server.use(morgan('combined'))
+server.use(logger);
+// server.use(validateUserId);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.get('/:id', validateUserId, (req, res) => {
+  res.send(`<h2>Let's write some middleware! </h2>`, req.hub);
+});
+
 //custom middleware
 
-function logger(req, res, next) {}
+// request method, request url, and a timestamp
+function logger(req, res, next) {
+  console.log(`Logger ${req.method} ${req.url} ${Date.now()} `); 
+  next();
+}
+
+
+
+// validateUserId validates the user id on every request that expects a user id parameter
+// if the id parameter is valid, store that user object as req.user
+// if the id parameter does not match any user id in the database, cancel the request and respond with status 400 and { message: "invalid user id" }
+
+// var d = new Date();
+// var n = d.getSeconds();
+//   if (n % 3 !== 0) { 
+//   console.log('ok', n);
+//   res.status(403).json({message : "You shall not pass!"});
+// } else {
+//   console.log('not ok', n);
+//   next();
+// };
+
+
+// for posts, user_id is required and must be id of an existing user
+
+function validateUserId(req, res, next){
+  const {user_id} = req.user_id;
+  
+  Hubs.findById(user_id)
+    .then(hub => {
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({message: 'hub id not found '});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Error retrieving hub'})
+    })
+};
+
+
+// // Write a middleware function called requiredBody. If req.body is not defined and is an empty object, it should cancel the request and send back a status 400 with the message "Please include request body".
+// function requiredBody(req, res, next){
+//   if (req.body && req.body !== "") {
+
+//   } else {
+//     res.status(400).json({message: 'Please include request body'})
+//   }
+// }
+
+
+
+
+
 
 module.exports = server;
+
+
